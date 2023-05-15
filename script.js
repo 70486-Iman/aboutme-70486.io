4227724b7d26325e83ab765a7124be548066d7a8
70486-Iman <133642446+70486-Iman@users.noreply.github.com>
2023-05-15 16:41:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+      const zeroFill = n => {
+        return ('0' + n).slice(-2);
+      }
+      const interval = setInterval(() => {
+
+        const now = new Date();
+        
+        const dateTime = zeroFill((now.getMonth() + 1)) + '/' + zeroFill(now.getUTCDate()) + '/' + now.getFullYear() + ' ' + zeroFill(now.getHours()) + ':' + zeroFill(now.getMinutes()) + ':' + zeroFill(now.getSeconds());
+
+        document.getElementById('date-time').innerHTML = dateTime;
+      }, 1000);
+
+
